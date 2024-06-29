e2b40ad027b5dbdc27422867a65ea658dfc2c23d
Nathaniel Ford <npford99@gmail.com>
2024-06-29 07:37:32 +0100

    Added current working on 'all tutorials'

---
+++ updated_code_mod_tutorials/VSTutorial - Tutorial Development (don't use)/VSTutorial/VSTutorialModSystem.cs
@@ -0,0 +1,28 @@
+﻿using Vintagestory.API.Common;
+using VSTutorial.Blocks;
+using VSTutorial.Items;
+
+namespace VSTutorial
+{
+    /*
+     * This is the entry point for the mod. ModSystems will be automatically detected and contain a load of useful functions for loading mods.
+     * Take a look at https://apidocs.vintagestory.at/api/Vintagestory.API.Common.ModSystem.html for more info.
+     */    
+    public class VSTutorialModSystem : ModSystem
+    {
+        /*
+         * This function is automatically called on the server and client when a world is loaded.
+         * Here you can register any blocks, items, entities, and other stuff that needs to be on  both the client and server sides.
+         */
+        public override void Start(ICoreAPI api)
+        {
+            /*
+             * This registers a new block class to be used in the game.
+             * When registering anything, the name should be "{modid}.{objectname}" in lowercase. This helps avoid duplicate IDs when using multiple mods.
+             * After registering, any blocks with the property: ' ("class":"vstutorial.trampoline") ' will use the BlockTrampoline class.
+             */
+            api.RegisterBlockClass(Mod.Info.ModID + ".trampoline", typeof(BlockTrampoline));
+            api.RegisterItemClass(Mod.Info.ModID + ".thornsblade", typeof(ItemThornsBlade));
+        }
+    }
+}
